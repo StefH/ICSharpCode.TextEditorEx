513f8c4591262e68e33c32cd00236a3660a57311
Stef <Stef.Heyenrath@gmail.com>
2014-08-07 09:41:21 +0200

    Add helper method SetTextAndRefresh
    Add helper method SetTextAndRefresh to easily set the text and refresh
    the control to update it's content.

--- ICSharpCode.TextEditorEx/TextEditorControlEx.cs
+++ ICSharpCode.TextEditorEx/TextEditorControlEx.cs
@@ -21,5 +21,16 @@ namespace ICSharpCode.TextEditor
 
             editactions[Keys.Control | Keys.G] = new GoToLineNumberAction();
         }
+
+        /// <summary>
+        /// Sets the text and refreshes the control.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        public void SetTextAndRefresh(string text)
+        {
+            ResetText();
+            Text = text;
+            Refresh();
+        }
     }
 }
