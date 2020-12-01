8da5b909530d5df4c33e0ce3d0f8babdfb5564ab
MysticBoy <mysticboy@live.com>
2020-12-01 14:38:01 +0800

    upgrade to .net 5.0

--- WinFormTestXmlEditor/Program.cs
+++ WinFormTestXmlEditor/Program.cs
@@ -11,6 +11,7 @@ namespace WinFormTestXmlEditor
         [STAThread]
         static void Main()
         {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
--- WinFormTestXmlEditor/Properties/AssemblyInfo.cs
+++ WinFormTestXmlEditor/Properties/AssemblyInfo.cs
@@ -4,12 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinFormTestXmlEditor")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinFormTestXmlEditor")]
-[assembly: AssemblyCopyright("Copyright © Stef Heyenrath 2014")]
+
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,15 +16,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("24bb212f-16e8-45e1-bb35-bf9b4825d69c")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
